2cbbcfab38ad8a0cb714d18b62f78d71e0e4502f
quisty <algoritm.gus@gmail.com>
2024-01-28 16:06:29 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(function () {
+  $('.slider').slick({
+    arrows: true,
+    dots: true,
+    centerMode: true,
+    slidesToShow: 1,
+    centerPadding: '50px',
+    speed: 1500,
+    autoplay: true,
+    autoplaySpeed: 800,
+  });
+});
